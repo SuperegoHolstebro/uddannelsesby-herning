ac0a2b892b9bee4b43cee1dcce79213f66e67d8e
emiliehjoellund <mail@emiliehjoellund.dk>
2024-10-22 10:49:17 +0200

    Created event signup form and added it to the page

--- src/app/(website)/begivenheder/[...slug]/page.tsx
+++ src/app/(website)/begivenheder/[...slug]/page.tsx
@@ -14,6 +14,8 @@ import { urlFor } from '~/sanity/lib/sanity.image'
 import Image from 'next/image'
 import { generatePageMetadata } from '~/utils/metadataUtils'
 import { stegaClean } from '@sanity/client/stega'
+import EventSignUpForm from '@/components/sections/EventSignUpForm'
+
 export default async function DynamicRoute({
   params: { slug: slugArray, locale },
 }: {
@@ -28,35 +30,45 @@ export default async function DynamicRoute({
 
   return (
     <PageContainer>
-      <Section variant="primary" paddingTop="none" paddingX="none" paddingBottom='none' className='h-screen/1.6'>
-        <Section paddingBottom='none' className="order-2 col-span-full sm:col-span-8 md:col-span-6 lg:col-span-6 xl:col-span-12 md:order-1 md:my-auto" tag={'div'}>
-          <div className='col-span-full'>
-            <Heading spacing='small'>{page.title}</Heading>
-            <Heading type='p' tag='p' spacing='default'>{formatDate(page.date)}</Heading>
-            <Paragraph>
-              {page.description}
-            </Paragraph>
+      <Section
+        variant="primary"
+        paddingTop="none"
+        paddingX="none"
+        paddingBottom="none"
+        className="h-screen/1.6"
+      >
+        <Section
+          paddingBottom="none"
+          className="order-2 col-span-full sm:col-span-8 md:col-span-6 lg:col-span-6 xl:col-span-12 md:order-1 md:my-auto"
+          tag={'div'}
+        >
+          <div className="col-span-full">
+            <Heading spacing="small">{page.title}</Heading>
+            <Heading type="p" tag="p" spacing="default">
+              {formatDate(page.date)}
+            </Heading>
+            <Paragraph>{page.description}</Paragraph>
           </div>
         </Section>
         <div className="order-1 col-span-full sm:col-span-8 md:col-span-6 lg:col-span-6 xl:col-span-12 md:order-2">
           <Image
-                className="object-cover h-full"
-                src={urlFor(page.image).dpr(2).url()}
-                alt=""
-                width={1920}
-                height={1080}
-                placeholder="blur"
-                blurDataURL={urlFor(page.image)
-                  .width(24)
-                  .height(24)
-                  .blur(10)
-                  .url()}
-                sizes="
+            className="object-cover h-full"
+            src={urlFor(page.image).dpr(2).url()}
+            alt=""
+            width={1920}
+            height={1080}
+            placeholder="blur"
+            blurDataURL={urlFor(page.image).width(24).height(24).blur(10).url()}
+            sizes="
                   (max-width: 768px) 100vw,
                   (max-width: 1200px) 50vw,
                   40vw"
-              />
+          />
         </div>
+      </Section>
+      {/* Include the EventSignUpForm component */}
+      <Section variant="primary">
+        <EventSignUpForm event={page} />
       </Section>
 
       {page.pageBuilder && <PageBuilder sections={page.pageBuilder} />}
@@ -64,10 +76,14 @@ export default async function DynamicRoute({
   )
 }
 
-export async function generateMetadata({ params }: { params: { slug: string[], } }) {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-  const slug = `${params.slug.join('/')}`;
-  const page = await loadPage(slug, 'da');
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string[] }
+}) {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
+  const slug = `${params.slug.join('/')}`
+  const page = await loadPage(slug, 'da')
 
-  return generatePageMetadata(page, baseUrl);
+  return generatePageMetadata(page, baseUrl)
 }
