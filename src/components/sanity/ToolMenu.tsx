e91421010487c1c9f80b3a603b3cdf69bd2e390e
emiliehjoellund <mail@emiliehjoellund.dk>
2024-10-23 10:11:26 +0200

    Added Schedules to the menu

--- src/components/sanity/ToolMenu.tsx
+++ src/components/sanity/ToolMenu.tsx
@@ -10,22 +10,22 @@ export function CustomToolMenu(props: ToolMenuProps) {
   const direction = isSidebar ? 'column' : 'row'
 
   // Filter out the tool with the title "Schedules"
-  const filteredTools = tools.filter(tool => tool.title !== 'Schedules')
-
+  /*   const filteredTools = tools.filter(tool => tool.title !== 'Schedules')
+   */
   return (
     <Flex gap={1} direction={direction}>
-      {filteredTools.map((tool) => (
-          <Button
-            as={ToolLink}
-            classID={tool.title}
-            icon={tool.icon || PlugIcon}
-            key={tool.name}
-            name={tool.name}
-            padding={2}
-            selected={tool.name === activeToolName}
-            text={(tool.title === 'Media' ? 'Medier' : tool.title)}
-            tone="positive"
-          />
+      {tools.map((tool) => (
+        <Button
+          as={ToolLink}
+          classID={tool.title}
+          icon={tool.icon || PlugIcon}
+          key={tool.name}
+          name={tool.name}
+          padding={2}
+          selected={tool.name === activeToolName}
+          text={tool.title === 'Media' ? 'Medier' : tool.title}
+          tone="positive"
+        />
       ))}
     </Flex>
   )
