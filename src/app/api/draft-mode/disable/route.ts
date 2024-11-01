f3520da91256fff37a13fe97b44ad103c73f83d6
Emilie Hjøllund <mail@emiliehjoellund.dk>
2024-11-01 14:21:10 +0100

    Update route.ts

--- src/app/api/draft-mode/disable/route.ts
+++ src/app/api/draft-mode/disable/route.ts
@@ -4,6 +4,6 @@ import { draftMode } from 'next/headers'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(request: NextRequest) {
-  ;(await draftMode()).enable() // Don't use await here
+  (await draftMode()).enable() // Don't use await here
   return NextResponse.redirect(new URL('/', request.url))
 }
