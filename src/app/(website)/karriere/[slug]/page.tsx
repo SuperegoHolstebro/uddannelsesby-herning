36f488dff6821d0e7dde92a4bfa00f3db5272afd
emiliehjoellund <mail@emiliehjoellund.dk>
2024-11-08 15:24:54 +0100

    styled company page

--- src/app/(website)/karriere/[slug]/page.tsx
+++ src/app/(website)/karriere/[slug]/page.tsx
@@ -20,6 +20,10 @@ import { getServerSession } from 'next-auth'
 import { authOptions } from '@/sanity/lib/authOptions'
 import { useSession } from 'next-auth/react'
 
+import Icon from '~/components/atoms/Icons'
+import { AdvancedButton } from '~/components/atoms/AdvancedButton'
+import { CallToAction2 } from '~/sanity/schemas/sections/CallToAction2'
+
 export interface UserProfile {
   name?: string
   email?: string
@@ -43,87 +47,102 @@ export default async function DynamicRoute({
 
   return (
     <PageContainer>
-      <Section>
-        <div className="col-span-full">
-          {page.name && <Heading>{page.name}</Heading>}
-
-          <div className="flex flex-row justify-between gap-12">
-            {page.image && (
-              <Image
-                className="object-cover"
-                src={urlFor(page.image).dpr(2).url()}
-                alt={page.altText || 'Billede af ' + page.title}
-                width={1920}
-                height={1080}
-                placeholder="blur"
-                blurDataURL={urlFor(page.image)
-                  .width(24)
-                  .height(24)
-                  .blur(10)
-                  .url()}
-                sizes="
-             (max-width: 768px) 100vw,
-             (max-width: 1200px) 50vw,
-             40vw"
-              />
-            )}
-            <div className="flex-col prose-p:pb-4">
-              {page.description && (
-                <>
-                  <Heading type="h6" tag="h6" spacing="none">
-                    Om os
-                  </Heading>
-                  <Paragraph spacing="none">{page.description}</Paragraph>
-                </>
-              )}
-              {page.address && (
-                <>
-                  <Heading type="h6" tag="h6" spacing="none">
-                    Adresse
-                  </Heading>
-                  <Paragraph>{page.address}</Paragraph>
-                </>
-              )}
-              {page.phone && (
-                <>
-                  <Heading type="h6" tag="h6" spacing="none">
-                    Telefon
-                  </Heading>
-                  <Paragraph>
-                    <a href={`tel:${page.phone}`}>{page.phone}</a>
-                  </Paragraph>
-                </>
-              )}
-              {page.email && (
-                <>
-                  <Heading type="h6" tag="h6" spacing="none">
-                    Email
-                  </Heading>
-                  <Paragraph>
-                    <a href={`mailto:${page.email}`}>{page.email}</a>
-                  </Paragraph>
-                </>
-              )}
-              {page.fields && (
-                <>
-                  <Heading type="h6" tag="h6" spacing="none">
-                    Fagområder
-                  </Heading>
-                  {/*    <ul>
-                    {page.fields.map((field: string, index: number) => (
-                      <li key={index}>{field}</li>
-                    ))}
-                  </ul> */}
-                </>
-              )}
-
-              {/*  <pre>{JSON.stringify(page, null, 2)}</pre> */}
-
-              {isUserAssignedToCompany && <EditButton />}
-            </div>
+      <Section
+        variant="primary"
+        paddingTop="none"
+        paddingBottom="none"
+        className="h-screen/3 bg-signal-pink"
+      >
+        <div className="flex flex-col justify-center col-span-full">
+          <div className="">
+            <Heading spacing="none">{page.name}</Heading>
+          </div>
+          <div className="flex col-span-full h-fit">
+            <Paragraph className="bg-mørk uppercase text-lys rounded-full px-2 py-1">
+              kategori{' '}
+            </Paragraph>
           </div>
         </div>
       </Section>
+
+      {/* info boxes */}
+      <Section paddingBottom="none" paddingTop="none" tag={'div'}>
+        {' '}
+        <div className="grid grid-cols-1 gap-4 text-center col-span-full sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 md:p-12 md:flex-row rounded-2xl">
+          <div className="flex flex-col items-center justify-start space-y-5 ">
+            <Icon type="calendar" className="w-8 h-8" />
+            <Heading type="h5" tag="h5" spacing="default">
+              {/* Use eventDateRange and pass the correct properties */}
+              hej{' '}
+            </Heading>
+            <Paragraph spacing="none">Dato</Paragraph>
+          </div>
+
+          <div className="flex flex-col items-center justify-start space-y-5 md:border-l md:border-grå md:pl-4">
+            <Icon type="clock" className="w-8 h-8" />
+            <Heading type="h5" tag="h5" spacing="default">
+              18.30-21.00
+            </Heading>
+            <Paragraph spacing="none">Tidspunkt</Paragraph>
+          </div>
+        </div>
+      </Section>
+
+      {/* image */}
+      <Section
+        variant="primary"
+        paddingTop="none"
+        paddingBottom="none"
+        className="col-span-full"
+      >
+        <div className="col-span-full h-screen/3">
+          <Image
+            className="object-cover h-full"
+            src={urlFor(page.image).dpr(2).url()}
+            alt=""
+            width={1920}
+            height={1080}
+            placeholder="blur"
+            blurDataURL={urlFor(page.image).width(24).height(24).blur(10).url()}
+            sizes="
+                  (max-width: 768px) 100vw,
+                  (max-width: 1200px) 50vw,
+                  40vw"
+          />
+        </div>
+      </Section>
+
+      <TextContainer>
+        {page.description && (
+          <>
+            <Heading type="h3" tag="h3">
+              Om {page.name}
+            </Heading>
+            <Paragraph>{page.description}</Paragraph>
+          </>
+        )}
+        {page.address && <Paragraph>Adresse: {page.address}</Paragraph>}
+        {page.email && (
+          <Paragraph>
+            Mail: <a href={`mailto:${page.email}`}>{page.email}</a>
+          </Paragraph>
+        )}
+        {page.phone && (
+          <Paragraph>
+            Tlf.: <a href={`tel:${page.phone}`}>{page.phone}</a>
+          </Paragraph>
+        )}
+
+        {page.fields && (
+          <>
+            <Heading type="h6" tag="h6" spacing="none">
+              Fagområder
+            </Heading>
+          </>
+        )}
+      </TextContainer>
+      {isUserAssignedToCompany && <EditButton />}
+      {page.pageBuilder && <PageBuilder sections={page.pageBuilder} />}
     </PageContainer>
   )
 }
