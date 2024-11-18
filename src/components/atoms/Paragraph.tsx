541895c1be00dce8ed5f45516996dc03917605c6
emiliehjoellund <mail@emiliehjoellund.dk>
2024-11-18 14:17:45 +0100

    Responsiveness

--- src/components/atoms/Paragraph.tsx
+++ src/components/atoms/Paragraph.tsx
@@ -68,7 +68,7 @@ const myPortableTextComponents = {
     myBlock: ({ children }) => <div className="my-block">{children}</div>,
   },
   marks: {
-    em: ({ children }) => (<em className="italic font-semibold ">{children}</em>),
+    em: ({ children }) => <em className="italic font-semibold">{children}</em>,
     link: ({ children, value }) => {
       const rel =
         value && value.href && !value.href.startsWith('/')
@@ -76,8 +76,10 @@ const myPortableTextComponents = {
           : undefined
       return (
         <a
-          className="font-bold underline transition-all custom-a text-green hover:text-purple"
+          className="font-bold underline transition-all custom-a text-superego-green hover:text-superego-purple"
           href={value.href}
+          target={value.blank ? '_blank' : '_self'}
+          title={children}
           rel={rel}
         >
           {children}
@@ -86,27 +88,88 @@ const myPortableTextComponents = {
     },
   },
   block: {
-    normal: ({ children }) => (<p className="mt-2 mb-4 font-sans custom-p text-regular ">{children}</p>),
-    h1: ({ children }) => (<h1 className="mt-8 mb-4 font-bold custom-h1 max-w-prose text-balance text-giant">{children}</h1>),
-    h2: ({ children }) => (<h2 className="mt-8 mb-4 font-bold custom-h2 max-w-prose text-balance text-huge">{children}</h2>),
-    h3: ({ children }) => (<h3 className="font-bold custom-h3 max-w-prose text-balance text-large">{children}</h3>),
-    h4: ({ children }) => (<h4 className="font-bold custom-h4 max-w-prose text-balance text-medium">{children}</h4>),
-    h5: ({ children }) => (<h5 className="font-bold custom-h5 max-w-prose text-balance text-increased"> {children} </h5>),
-    h6: ({ children }) => (<h6 className="font-bold custom-h6 max-w-prose text-balance text-small"> {children} </h6>),
-    ul: ({ children }) => (<ul className="ml-5 list-disc list-outside custom-ul"> {children} </ul>),
-    ol: ({ children }) => (<ol className="">{children}</ol>),
-    strong: ({ children }) => (<strong className="font-bold custom-strong">{children}</strong>),
+    normal: ({ children }) => (
+      <p className="font-sans custom-p text-regular ">{children}</p>
+    ),
+    h1: ({ children }) => (
+      <h1 className="mt-8 mb-4 font-bold custom-h1 max-w-prose text-balance text-giant">
+        {children}
+      </h1>
+    ),
+    h2: ({ children }) => (
+      <h2 className="mt-8 mb-4 font-bold custom-h2 max-w-prose text-balance text-huge">
+        {children}
+      </h2>
+    ),
+    h3: ({ children }) => (
+      <h3 className="font-bold custom-h3 max-w-prose text-balance text-large">
+        {children}
+      </h3>
+    ),
+    h4: ({ children }) => (
+      <h4 className="font-bold custom-h4 max-w-prose text-balance text-medium">
+        {children}
+      </h4>
+    ),
+    h5: ({ children }) => (
+      <h5 className="font-bold custom-h5 max-w-prose text-balance text-increased">
+        {' '}
+        {children}{' '}
+      </h5>
+    ),
+    h6: ({ children }) => (
+      <h6 className="font-bold custom-h6 max-w-prose text-balance text-small">
+        {' '}
+        {children}{' '}
+      </h6>
+    ),
+    ul: ({ children }) => (
+      <ul className="ml-5 list-disc list-outside custom-ul"> {children} </ul>
+    ),
+    ol: ({ children }) => <ol className="">{children}</ol>,
+    strong: ({ children }) => (
+      <strong className="font-bold custom-strong">{children}</strong>
+    ),
     em: ({ children }) => <em className="italic custom-em">{children}</em>,
-    code: ({ children }) => (<code className="px-1 py-0 bg-gray-200 rounded custom-code">{children}</code>),
+    code: ({ children }) => (
+      <code className="px-1 py-0 bg-gray-200 rounded custom-code">
+        {children}
+      </code>
+    ),
     pre: ({ children }) => <pre className="custom-pre">{children}</pre>,
     sub: ({ children }) => <sub className="custom-sub">{children}</sub>,
     sup: ({ children }) => <sup className="custom-sup">{children}</sup>,
-    blockquote: ({ children }) => (<blockquote className="italic ">{children}</blockquote>),
+    blockquote: ({ children }) => (
+      <blockquote className="pl-4 italic border-l-2 border-superego-grey/50 ">
+        {children}
+      </blockquote>
+    ),
   },
+
   list: {
-    bullet: ({ children }) => <ul className="prose-li:ml-6 prose-li:list-outside prose-li:list-disc prose-li:list-item">{children}</ul>,
-    number: ({ children }) => <ol className="prose-li:ml-6 prose-li:list-decimal prose-li:list-outside prose-li:list-item">{children}</ol>,
-  }
+    bullet: ({ children }) => (
+      <ul className="prose-li:ml-6 prose-li:list-disc prose-li:list-item">
+        {children}
+      </ul>
+    ),
+    number: ({ children }) => (
+      <ol className="prose-li:ml-6 prose-li:list-decimal prose-li:list-item">
+        {children}
+      </ol>
+    ),
+  },
+  listItem: {
+    bullet: ({ children }) => (
+      <li className="py-2 list-disc list-outside custom-li marker:text-red-500 marker:size-7 list-image-star">
+        {children}
+      </li>
+    ),
+    number: ({ children }) => (
+      <li className="py-2 list-decimal list-outside bg-blue-500 custom-li">
+        {children}
+      </li>
+    ),
+  },
 }
 
 const TextComponent = (props) => {
