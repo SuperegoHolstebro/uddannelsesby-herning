da5bf2a0406f49b952aec74f52f451f7d85f1f99
emiliehjoellund <mail@emiliehjoellund.dk>
2024-12-05 08:39:53 +0100

    refactor: Move useWindow hook to hooks directory

--- src/hooks/useWindow.ts
+++ src/hooks/useWindow.ts
@@ -1,19 +1,19 @@
 import { useEffect, useState } from 'react'
 
 export default function useWindow() {
-  const [dimension, setDimension] = useState({width: 0, height: 0})
+  const [dimension, setDimension] = useState({ width: 0, height: 0 })
 
   const resize = () => {
     setDimension({
       width: window.innerWidth,
-      height: window.innerHeight
+      height: window.innerHeight,
     })
   }
 
   useEffect(() => {
-    resize();
-    window.addEventListener("resize", resize);
-    return () => window.removeEventListener("resize", resize);
+    resize()
+    window.addEventListener('resize', resize)
+    return () => window.removeEventListener('resize', resize)
   }, [])
 
   return { dimension }
