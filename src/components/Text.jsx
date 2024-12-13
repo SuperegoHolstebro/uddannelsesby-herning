98969cfbfc4fcdbfbdeedcfa83414f28ab1a0016
emiliehjoellund <mail@emiliehjoellund.dk>
2024-12-14 00:42:35 +0100

    added new se det for dig animation

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -1,32 +1,29 @@
 'use client'
 import React from 'react'
 import Image from 'next/image'
-import { DotLottieReact } from '@lottiefiles/dotlottie-react'
+import { Player, Controls } from '@lottiefiles/react-lottie-player'
+
 export default function Text({ videoSrc, textContent }) {
   return (
     <>
+      <div className="absolute inset-0 z-10 pointer-events-none size-full bg-gradient-to-b from-transparent via-transparent to-mørk/40" />
       <video
         autoPlay
         loop
         muted
         playsInline
-        className="absolute inset-0 object-cover w-full h-full bg-cover pointer-events-none"
+        className="absolute inset-0 object-cover w-full h-screen bg-cover pointer-events-none"
       >
         <source src={videoSrc} type="video/mp4" />
       </video>
-      <div className="absolute right-0 z-10 w-1/3 h-screen py-16 pointer-events-none xl:pb-0 top-28 md:top-20 xl:top-0 ">
-        <DotLottieReact
-          src="lottie/SE-DET-FOR-DIG.lottie"
-          loop
+      <div className="absolute right-0 z-10 py-0 pointer-events-none xl:pb-0 top-28 md:top-20 xl:top-0 scale-[.9] ">
+        <Player
           autoplay
-          className="*:bg-cover bg-cover size-full"
-        />
-        {/*   <h1 className="px-8 py-4 pb-8 font-light text-right pointer-events-none text-giant max-w-prose font-display text-balance xl:pt-8 xl:pb-2">
-          <div className="font-light font-display text-giant"> se </div>
-          <div className="font-light font-display text-giant"> dET </div>
-          <div className="font-light font-display text-giant"> FOr </div>
-          <div className="font-light font-display text-giant"> diG </div>
-        </h1> */}
+          loop
+          hover
+          src="/lottie/SE-DET-FOR-DIG.json"
+          className="bg-cover "
+        ></Player>
       </div>
 
       <div className="absolute bottom-0 left-0 z-10 pb-8 pl-6 font-sans pointer-events-none md:pl-32 lg:pl-36 text-lys xs:max-w-md sm:max-w-md md:max-w-xl lg:max-w-3xl">
