e6bdb9c0edd441fec9f72f32a90e8ceab8297f72
emiliehjoellund <mail@emiliehjoellund.dk>
2024-12-16 03:46:40 +0100

    added a simple config file

---
+++ config.ts
@@ -0,0 +1,10 @@
+const Appconfig = {
+  i18n: {
+    locales: [
+      { id: 'en', title: 'English' },
+      { id: 'da', title: 'Danish' },
+    ],
+    defaultLocaleId: 'da',
+  },
+}
+export default Appconfig
