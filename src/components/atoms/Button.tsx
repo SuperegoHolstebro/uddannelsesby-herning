dc704b2b1aacf020e773c367db3ed9ad9c946e50
emiliehjoellund <mail@emiliehjoellund.dk>
2024-12-16 01:34:55 +0100

    refactor: Add title prop to Button component

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -83,6 +83,7 @@ const Button = forwardRef(
                   ),
             ),
           )}
+          title={children}
           target={
             !isPhoneLink(link) && !isEmailLink(link) && link.blank
               ? '_blank'
