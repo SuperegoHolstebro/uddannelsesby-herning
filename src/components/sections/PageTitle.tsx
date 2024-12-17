3c20b9b78f58fb93a4aa154f71f92965a59bbc99
emiliehjoellund <mail@emiliehjoellund.dk>
2024-12-17 21:53:18 +0100

    refactor: Update PageTitle component CSS for better responsiveness

--- src/components/sections/PageTitle.tsx
+++ src/components/sections/PageTitle.tsx
@@ -35,11 +35,11 @@ const PageTitle = ({ data }) => {
       id={clean(data?.SectionSettings?.anchor?.current)}
       paddingTop="none"
       paddingBottom="none"
-      className="bg-mørk text-lys pt-36 md:pb-20 pb-40 sm:pb-0 sm:pt-44 pr-0 overflow-y-visible overflow-x-clip "
+      className="bg-mørk text-lys pt-36 pb-0 sm:pt-44 pr-0 overflow-y-visible overflow-x-clip "
       paddingX="left"
     >
       <div className="col-span-full">
-        <div className="w-full pb-8 xs:w-2/3 sm:pb-12 sm:w-2/3 md:w-1/2 ">
+        <div className="w-full pb-8 sm:pb-12 sm:w-2/3 md:w-1/2 ">
           <FadeUp delay={0.25}>
             <Heading
               type="h1"
@@ -50,7 +50,7 @@ const PageTitle = ({ data }) => {
           </FadeUp>
         </div>
 
-        <div className="relative flex justify-start h-full max-w-3xl pr-0 ml-auto -mb-12 -mr-6 space-x-4 xs:-mr-12 w-fit sm:w-full flex-nowrap sm:h-auto sm:justify-end md:space-x-6 xl:max-w-6xl lg:max-w-4xl ">
+        <div className="relative flex justify-start h-[inherit] md:h-auto max-w-3xl pr-0 ml-auto -mb-12 -mr-6 space-x-4 xs:-mr-12 w-fit sm:w-full flex-nowrap sm:h-auto sm:justify-end md:space-x-6 xl:max-w-6xl lg:max-w-4xl ">
           {data.images.map((image, index) => (
             <div
               className={`${imageStyles} overflow-hidden *:size-full`}
@@ -69,7 +69,7 @@ const PageTitle = ({ data }) => {
               />
             </FadeUp>
           </span>
-          <span className="absolute bottom-4 -left-4 xs:bottom-24 xs:-left-20">
+          <span className="absolute bottom-4 -left-4 xs:bottom-24 xs:-left-12 md:-left-20">
             <FadeUp delay={0.725}>
               <Symbol
                 type={data.symbolPickerLeft?.icon}
