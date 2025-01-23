54c014c268798516b03b067e7c0da745bc6af585
Kasper Buchholtz <117350672+Kasper-Buchholtz@users.noreply.github.com>
2025-01-23 10:18:06 +0100

    refactor: Simplify CookiePage component structure by removing unnecessary containers

--- src/app/(website)/cookiepolitik/page.tsx
+++ src/app/(website)/cookiepolitik/page.tsx
@@ -1,29 +1,18 @@
 import React from 'react'
 import Script from 'next/script'
-import PageContainer from '@/components/PageContainer'
-import TextContainer from '@/components/sections/textContainer'
-import Heading from '@/components/atoms/Heading'
-
-export const dynamic = 'force-dynamic'
 
 const CookiePage = () => {
   return (
-    <PageContainer>
-      <TextContainer>
-        <div>
-          <Heading>Cookiepolitik</Heading>
-          <div>
-            <Script
-              src="https://policy.app.cookieinformation.com/cid.js"
-              data-culture="DA"
-              id="CookiePolicy"
-              strategy="afterInteractive"
-              type="text/javascript"
-            />
-          </div>
-        </div>
-      </TextContainer>
-    </PageContainer>
+    <>
+      <Script
+        id="CookiePolicy"
+        src="https://policy.app.cookieinformation.com/cid.js"
+        data-culture="DA"
+        type="text/javascript"
+        strategy="afterInteractive"
+      />
+    </>
+
   )
 }
 
